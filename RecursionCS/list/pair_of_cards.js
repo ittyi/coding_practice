6b6e3faa186bf3db3c2f898b7b0ca5aa9ff5cf4b
ittyi <vioiv.k4@gmail.com>
2022-01-06 20:24:53 +0900

    pair_of_cards更新

--- RecursionCS/list/pair_of_cards.js
+++ RecursionCS/list/pair_of_cards.js
@@ -1,108 +1,164 @@
+// カード、デッキ、ディーラーをそれぞれ 1 から自分の力で作成し、「Pair of Cards」を設計
+// ♣6(6)の形式
+class Card{
+    constructor(mark, value, intValue){
+        this.mark = mark;
+        this.value = value;
+        this.intValue = intValue;
+    }
+
+    printCard(){
+        return this.mark + this.value + "(" + this.intValue + ")";
+    }
+}
+
+// Deck 4*13で52枚のデッキを作る
+class Deck{
+    deckGenerator(){
+        let allMark = ['♣', '♥', '♠', '♦'];
+        let allNumber = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+        let deck = [];
+        for (let i in allMark){
+            for (let j in allNumber){
+                deck.push(new Card(allMark[i], allNumber[j], Number(j)+1));
+            }
+        }
+        return deck;
+    }
+}
+
+class Dealer{
+    shuffle(deck){
+        for (let i in deck){
+            let randamIndex = Math.floor(Math.random() * 52);
+            let tmp = deck[i];
+            deck[i] = deck[randamIndex];
+            deck[randamIndex] = tmp;
+        }
+        return deck;
+    }
+
+    distribution(shuffle_deck, num_players){
+        let players = [];
+        for (let i = 0; i < num_players; i++){
+            let hand_cards = [];
+            for (let j = 0; j < 5; j++){
+                hand_cards.push(shuffle_deck[0])
+                shuffle_deck.shift();
+            }
+            players.push(hand_cards);
+        }
+        return players;
+    }
+}
+
+let deck1 = new Deck();
+let deck2 = deck1.deckGenerator();
+let dealer = new Dealer();
+let shuffle_deck = dealer.shuffle(deck2);
+let players = dealer.distribution(shuffle_deck, 2);
+// console.log("---これできたらOK！--")
+// console.log(players)
+// console.log("-----あとは、加工して関数に渡すだけ！-----")
+
+// なんか変なことしてる 笑
+// 仕方ないから加工します。
+let player1 = [];
+for (let i = 0; i < players[0].length; i++){
+    // console.log(i, players[0][i])
+    player1.push(players[0][i]["mark"] + players[0][i]["value"])
+}
+let player2 = [];
+for (let i = 0; i < players[1].length; i++){
+    // console.log(i, players[1][i])
+    player2.push(players[1][i]["mark"] + players[1][i]["value"])
+}
+console.log("player1: ", player1)
+console.log("player2: ", player2)
+
+console.log(winnerPairOfCards(player1, player2))
+
+
 // 作り直した。テストケースまでclear
-// このケースもclear["♠2","♠4","♥K","♥A","♣10"],["♣Q","♦3","♣J","♠A","♦8"]
+// // ["♠2","♠4","♥K","♥A","♣10"],["♣Q","♦3","♣J","♠A","♦8"]
 function winnerPairOfCards(player1,player2){
     // hashmapの作成 ※この時、マークは関係ないので除外
-    let hashMapNum1 = {};
-    for (i in player1){
-        if (hashMapNum1[player1[i].slice(1)] >= 1) hashMapNum1[player1[i].slice(1)] += 1;
-        else hashMapNum1[player1[i].slice(1)] = 1;
-    }
+    let hashMapNum1 = hashMapming(player1);
+    let hashMapNum2 = hashMapming(player2);
 
-    let hashMapNum2 = {};
-    for (i in player2){
-        if (hashMapNum2[player2[i].slice(1)] >= 1) hashMapNum2[player2[i].slice(1)] += 1;
-        else hashMapNum2[player2[i].slice(1)] = 1;
-    }
-
-    console.log("hashMapNum1", hashMapNum1)
-    console.log("hashMapNum2", hashMapNum2)
     // 2 < 3 < 4 < 5 < 6 … J < Q < K < A 
     // あと比較したいのは、ランクの強さを、『枚数が多いものから。』
     // 一旦、後に使うためのデータ加工をする。 それぞれ手札の[ランク（文字), 実質強さ(indexで表現), 持ってる枚数]
-    let rankAscending = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    let byRank1 = [];
-    for (i in rankAscending){
-        if (hashMapNum1[rankAscending[i]] != undefined){
-            byRank1.push([rankAscending[i], i , hashMapNum1[rankAscending[i]]]);
-        }
-    }
-
-    let byRank2 = [];
-    for (i in rankAscending){
-        if (hashMapNum2[rankAscending[i]] != undefined){
-            byRank2.push([rankAscending[i], i , hashMapNum2[rankAscending[i]]]);
-        }
-    }
-    console.log("byRank1: ", byRank1) 
-    console.log("byRank2: ", byRank2)
+    let byRank1 = dataProcessing(hashMapNum1);
+    let byRank2 = dataProcessing(hashMapNum2);
 
     // 一番最初の勝敗判定：最大数の多さ 
     // テスト例：["♣4","♥7","♥7","♥7","♣J"],["♥7","♥7","♣K","♠Q","♠Q"]
-    // ["♥3","♠9","♦3","♣Q","♦A"],["♥4","♥3","♠10","♦3","♥4"]
     for (let i = 4; i > 0; i--){
-        let compMaxNum1 = 0;
-        let compMaxRank1 = 0;
-        for (let j = 0; j < byRank1.length; j++){
-            if (i == byRank1[j][2] && compMaxRank1 < Number(byRank1[j][1])){
-                compMaxNum1 = byRank1[j][2];
-                compMaxRank1 = Number(byRank1[j][1]);
-            } else if (i == byRank1[j][2]){ // このケースに対処["♥2","♠A","♦6"],["♥2","♥2","♥3"]
-                compMaxNum1 = byRank1[j][2];
-                compMaxRank1 = Number(byRank1[j][1]);
-            }
-            console.log(i, j);
-            console.log("compMaxNum1: ", compMaxNum1);
-            console.log("compMaxRank1: ", compMaxRank1);
-        }
-
-        let compMaxNum2 = 0;
-        let compMaxRank2 = 0;
-        for (let j = 0; j < byRank2.length; j++){
-            if (i == byRank2[j][2] && compMaxRank2 < Number(byRank2[j][1])){
-                compMaxNum2 = byRank2[j][2];
-                compMaxRank2 = Number(byRank2[j][1]);
-            } else if (i == byRank2[j][2]){ // このケースに対処["♥2","♠A","♦6"],["♥2","♥2","♥3"]
-                compMaxNum2 = byRank2[j][2];
-                compMaxRank2 = Number(byRank2[j][1]);
-            }
-            console.log(i, j);
-            console.log("compMaxNum2: ", compMaxNum2);
-            console.log("compMaxRank2: ", compMaxRank2);
-        }
+        let compMaxNumRank1 = compMax(byRank1, i);
+        let compMaxNumRank2 = compMax(byRank2, i);
 
         // 勝敗判定
-        if (compMaxNum1 > compMaxNum2) return "player1";
-        else if (compMaxNum1 < compMaxNum2) return "player2";
-        else if (compMaxRank1 > compMaxRank2) return "player1";
-        else if (compMaxRank1 < compMaxRank2) return "player2";
+        if (compMaxNumRank1[0] > compMaxNumRank2[0]) return "player1";
+        else if (compMaxNumRank1[0] < compMaxNumRank2[0]) return "player2";
+        else if (compMaxNumRank1[1] > compMaxNumRank2[1]) return "player1";
+        else if (compMaxNumRank1[1] < compMaxNumRank2[1]) return "player2";
     }
 
     // 上記も同じ場合、次に枚数の多いカードを同様に比べ、勝敗が決まるまですべてのカードを比べる
     // ["♥9","♠8","♦7","♣6","♦5"],["♥9","♥8","♠7","♦6","♥4"]
     // ["♥3","♠4","♦5","♣6","♦7"],["♥2","♥3","♠5","♦6","♥7"]  等々
-    console.log("byRank1: ", byRank1) 
-    console.log("byRank2: ", byRank2)
     let compRank1 = [];
     for (let i = 0; i < byRank1.length; i++){
         compRank1.push(Number(byRank1[i][1]));
-        // console.log("compRank1: ", compRank1)
     }
     let compRank2 = [];
     for (let i = 0; i < byRank2.length; i++){
         compRank2.push(Number(byRank2[i][1]));
-        // console.log("compRank2: ", compRank2)
     }
-    
+
     compRank1.sort((a,b) => a-b);
     compRank2.sort((a,b) => a-b);
-    console.log("compRank1: ", compRank1)
-    console.log("compRank2: ", compRank2)
-    
     for (let i = compRank1.length; i >= 0; i--){
-        console.log(compRank1[i], compRank2[i])
         if (compRank1[i] > compRank2[i]) return "player1";
         else if (compRank1[i] < compRank2[i]) return "player2";
     }
     return "draw";
 }
 
+function hashMapming(player){
+    let hashMapNum = {};
+    for (i in player){
+        if (hashMapNum[player[i].slice(1)] >= 1) hashMapNum[player[i].slice(1)] += 1;
+        else hashMapNum[player[i].slice(1)] = 1;
+    }
+    return hashMapNum;
+}
+
+function dataProcessing(hashMapNum){
+    let rankAscending = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+    let byRank = [];
+    for (i in rankAscending){
+        if (hashMapNum[rankAscending[i]] != undefined){
+            byRank.push([rankAscending[i], i , hashMapNum[rankAscending[i]]]);
+        }
+    }
+    return byRank;// それぞれ手札の[ランク（文字), 実質強さ(indexで表現), 持ってる枚数]の二次元配列
+}
+
+function compMax(byRank, i){
+    let compMaxNum = 0;
+    let compMaxRank = 0;
+    for (let j = 0; j < byRank.length; j++){
+        if (i == byRank[j][2] && compMaxRank < Number(byRank[j][1])){
+            compMaxNum = byRank[j][2];
+            compMaxRank = Number(byRank[j][1]);
+        } else if (i == byRank[j][2]){ // このケースに対処["♥2","♠A","♦6"],["♥2","♥2","♥3"]
+            compMaxNum = byRank[j][2];
+            compMaxRank = Number(byRank[j][1]);
+        }
+    }
+    let comp = [compMaxNum, compMaxRank];
+    return comp;
+}
